2b1b77c0239ba6d2e3eb42baade40054954316a8
Sandra Latt <slatt@ucsc.edu>
2024-11-17 20:02:17 -0800

    centered and fixed basic functionality
    fixed check/strike

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ for (i = 0; i < myNodelist.length; i++) {
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
   }
@@ -21,8 +21,8 @@ for (i = 0; i < close.length; i++) {
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
+list.addEventListener('click', function (ev) {
+  if (ev.target.tagName === 'li') {
     ev.target.classList.toggle('checked');
   }
 }, false);
@@ -36,7 +36,7 @@ function newElement() {
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("listCell").appendChild(li);
+    document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
 
@@ -47,7 +47,7 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
     }
